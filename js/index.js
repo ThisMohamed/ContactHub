b21fb5f4351adcb5e62bc1ba9bc98dcbd953df74
ThisMohamed <m77md.107@gmail.com>
2025-12-22 19:29:41 +0200

    Update error message for invalid phone number to include additional valid formats

--- js/index.js
+++ js/index.js
@@ -487,7 +487,7 @@ function UpdateContact(){
         } else {
             Swal.fire({
                 title: "Invalid Phone Number!",
-                text: "Please enter a valid Egyptian phone number (e.g., 01012345678 or +201012345678)",
+                text: "Please enter a valid Egyptian phone number (e.g., 01012345678 , +201012345678 or 00201012345678)",
                 icon: "error",
                 draggable: true
             });
